43a82386e847b0ad98898b62ffa87672900d6305
User Evg-a <to270213@yandex.ru>
2025-10-12 20:04:03 +0300

    реализация сохранение менеджера в файл

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import taskmanager.FileBackedTaskManager;
+/*import taskmanager.FileBackedTaskManager;
 import taskmanager.Managers;
 import taskmanager.TaskManager;
 import tasks.Status;
@@ -7,12 +7,12 @@ import tasks.Task;
 import java.io.File;
 import java.io.IOException;
 
-import static tasks.TypeTask.TASK;
+import static tasks.TypeTask.TASK;*/
 
 public class Main {
 
     public static void main(String[] args) {
-        try {
+        /* try {
             File file = File.createTempFile("file", ".csv");
             System.out.println("Временный файл создан: " + file.getAbsolutePath());
         } catch (IOException e) {
@@ -21,16 +21,24 @@ public class Main {
 
         TaskManager manager = Managers.getDefault();
         FileBackedTaskManager fileManager = new FileBackedTaskManager(new File("file"));
+
         Task task1 = new Task (4, TASK, "nameTask1", "descriptionTask1", Status.NEW);
         Task task2 = new Task(4, TASK, "nameTask2", "descriptionTask2", Status.DONE);
+
         manager.addTask(task1);
+
         fileManager.addTask(task1);
+
         manager.addTask(task2);
+
         fileManager.addTask(task2);
+
         System.out.println(manager.getTasks());
         System.out.println(fileManager.getTasks());
+
         FileBackedTaskManager restoredManager = fileManager.loadFromFile(new File("file"));
-        System.out.println(restoredManager.getTasks());
+
+        System.out.println(restoredManager.getTasks());*/
     }
 }
 
